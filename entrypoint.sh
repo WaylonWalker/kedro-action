d4969510602795707cc8c30210cd20b4950f16a3
Waylon Walker <quadmx08@gmail.com>
2020-03-10 17:32:11 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,7 @@ push_to_branch() {
         git checkout --orphan $target_branch
         fi
 	
+        rm -rf ./*
 	cp -r $deploy_directory .
         
         DIRTY=$(git status --short | wc -l)
